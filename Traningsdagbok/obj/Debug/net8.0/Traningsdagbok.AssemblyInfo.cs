b524c58ba6b86233a88b785744681588dda02fea
Sorin909 <Sorin.gheorghe@infosolutions.se>
2024-01-29 11:39:46 +0100

    secound

--- Traningsdagbok/obj/Debug/net8.0/Traningsdagbok.AssemblyInfo.cs
+++ Traningsdagbok/obj/Debug/net8.0/Traningsdagbok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traningsdagbok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da04e03a9dced1005fb32f90eab38ed4b7b9c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traningsdagbok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traningsdagbok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
